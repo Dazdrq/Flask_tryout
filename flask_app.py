a1eb4e93f7089756bf72a795398212952b2f5cdd
Nick Lambrechts <nicklambrechts1984@hotmail.com>
2026-02-19 20:40:26 +0100

    Foute code vervangen

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,11 @@
 #Opdracht 3
 from flask import Flask
 
-app=Flask[__name__]
-if __name__ == '__main__':
-    app.run(port=5000,debuf=True)
+app=Flask(__name__)
 
 @app.route('/')
 def home():
     return"<p>Welkom op mijn nieuwe site</p>"
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
